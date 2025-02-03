9bdc880afe76acc39a855cdde2e5344d8cb31274
Easton <eastonmullins@usf.edu>
2025-02-02 20:54:40 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+let prices = [10, 20, 30, 40, 50];
+console.log("Prices: ", prices);
+
+prices.push (60); //Added 60 as price
+console.log("Added Price: ", prices)
+
+prices.shift (); //Removes first value from array
+console.log("Removed First Price: ", prices)
+//Task 1 - Product Price Management
